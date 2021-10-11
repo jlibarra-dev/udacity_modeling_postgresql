4cd86ea73a76ac640602989bcc93a238101a8669
Juan Lucas Ibarra <juanlucas2000@hotmail.com>
2021-10-11 14:38:06 -0500

    Fixes for queries

--- sql_queries.py
+++ sql_queries.py
@@ -10,12 +10,12 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
    songplay_id SERIAL PRIMARY KEY,
-   start_time timestamp,
-   user_id int,
+   start_time timestamp NOT NULL,
+   user_id int NOT NULL,
    level varchar,
-   song_id varchar,
-   artist_id varchar,
-   session_id int,
+   song_id varchar NOT NULL,
+   artist_id varchar NOT NULL,
+   session_id int NOT NULL,
    location varchar,
    user_agent varchar
 )
@@ -23,25 +23,25 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
    user_id int PRIMARY KEY,
-   first_name varchar,
-   last_name varchar,
+   first_name varchar NOT NULL,
+   last_name varchar NOT NULL,
    gender varchar,
-   level varchar
+   level varchar NOT NULL
 )
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
    song_id varchar PRIMARY KEY,
-   title varchar,
+   title varchar NOT NULL,
    artist_id varchar,
    year int,
-   duration float
+   duration float NOT NULL
 )
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
    artist_id varchar PRIMARY KEY,
-   name varchar,
+   name varchar NOT NULL,
    location varchar,
    latitude float,
    longitude float
@@ -50,49 +50,59 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
    start_time timestamp PRIMARY KEY,
-   hour int,
-   day int,
-   week int,
-   month int,
-   year int,
-   weekday int
+   hour int NOT NULL,
+   day int NOT NULL,
+   week int NOT NULL,
+   month int NOT NULL,
+   year int NOT NULL,
+   weekday int NOT NULL
 )
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s);
+songplay_table_insert = ("""INSERT INTO 
+songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (songplay_id)
+DO UPDATE SET level = EXCLUDED.level;
 """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
+user_table_insert = ("""INSERT INTO 
+users (user_id, first_name, last_name, gender, level) 
 VALUES (%s, %s, %s, %s, %s) 
 ON CONFLICT (user_id)
-DO UPDATE SET  (first_name, last_name, gender, level) = (EXCLUDED.first_name, EXCLUDED.last_name, EXCLUDED.gender, EXCLUDED.level)
+DO UPDATE SET level = EXCLUDED.level;
 """)
 
-song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
+song_table_insert = ("""INSERT INTO 
+songs (song_id, title, artist_id, year, duration) 
 VALUES (%s, %s, %s, %s, %s) 
 ON CONFLICT (song_id)
-DO UPDATE SET  (title, artist_id, year, duration) = (EXCLUDED.title, EXCLUDED.artist_id, EXCLUDED.year, EXCLUDED.duration)
+DO NOTHING;
 """)
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+artist_table_insert = ("""INSERT INTO 
+artists (artist_id, name, location, latitude, longitude) 
 VALUES (%s, %s, %s, %s, %s) 
 ON CONFLICT (artist_id)
-DO UPDATE SET  (name, location, latitude, longitude) = (EXCLUDED.name, EXCLUDED.location, EXCLUDED.latitude, EXCLUDED.longitude)
+DO NOTHING;
 """)
 
 
-time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
+time_table_insert = ("""INSERT INTO 
+time (start_time, hour, day, week, month, year, weekday) 
 VALUES (%s, %s, %s, %s, %s, %s, %s) 
 ON CONFLICT (start_time)
-DO UPDATE SET  (hour, day, week, month, year, weekday) = (EXCLUDED.hour, EXCLUDED.day, EXCLUDED.week, EXCLUDED.month, EXCLUDED.year, EXCLUDED.weekday)
+DO NOTHING;
 """)
 
 # FIND SONGS
 
-song_select = ("""SELECT s.song_id, a.artist_id FROM songs s INNER JOIN artists a ON s.artist_id=a.artist_id WHERE s.title = %s AND a.name = %s AND s.duration = %s;
+song_select = ("""SELECT s.song_id, a.artist_id 
+FROM songs s 
+INNER JOIN artists a ON s.artist_id=a.artist_id 
+WHERE s.title = %s AND a.name = %s AND s.duration = %s;
 """)
 
 # QUERY LISTS
